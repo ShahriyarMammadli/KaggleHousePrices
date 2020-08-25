b4ce7841445069ae163f497978e6e5ea57e58219
Shahriyar Mammadli <smammadli@pasha-holding.ph>
2020-08-25 20:20:18 +0400

    first model is built

--- helperFunctions.py
+++ helperFunctions.py
@@ -4,55 +4,53 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+import numpy as np
 
 # This function puts 'None' in place of NAs in specific variables
 def fillNANone(df, vars):
     for var in vars:
-        df[var] = df.fillna("None", inplace=True)
+        df[var] = df[var].fillna("None")
     return df
 
 # Fill the missing value with most frequent value in the variable
 def fillWithMostFrequent(df, vars):
     for var in vars:
-        df[var] = df.fillna(df[var].mode()[0], inplace=True)
+        df[var] = df[var].fillna(df[var].mode()[0])
     return df
 
 # Fill with 'Other'
 def fillNAOther(df, vars):
     for var in vars:
-        df[var] = df.fillna('Other', inplace=True)
+        df[var] = df[var].fillna('Other')
     return df
 
 # Fill with 0
 def fillNAZero(df, vars):
     for var in vars:
-        df[var] = df.fillna(0, inplace=True)
+        df[var] = df[var].fillna(0)
     return df
 
 # Ordinal Encoding
-def ordinalEncoding(df):
-    return df.apply(LabelEncoder().fit_transform)
+def ordinalEncoding(df, vars):
+    for var in vars:
+        df[var] = LabelEncoder().fit_transform(df[var])
+    return df
 
 # Random Forest Model
 def rfModel(trainDf, testDf, targetVar):
-    rf = RandomForestRegressor(n_estimators=100)
+    rf = RandomForestRegressor()
     # Fit data
     rf = rf.fit(trainDf.drop(targetVar, 1), trainDf[targetVar])
     return rf, rf.predict(testDf.drop(targetVar, 1))
 
 # Gradient Boosting Model
 def gbModel(trainDf, testDf, targetVar):
-    gb = GradientBoostingRegressor(n_estimators=100, max_features='sqrt')
+    gb = GradientBoostingRegressor()
     # Fit data
     gb = gb.fit(trainDf.drop(targetVar, 1), trainDf[targetVar])
     return gb, gb.predict(testDf.drop(targetVar, 1))
 
-# Calculate R-squared
-def calcR2(actual, predicted):
-    return r2_score(actual, predicted)
-
 # Calculate MAPE
-def calcR2(actual, predicted):
-    return mean_absolute_error(actual, predicted)
+def MAPE(actual, predicted):
+    return (np.mean(np.abs((actual - predicted) / actual)) * 100).round(1)
--- solution.py
+++ solution.py
@@ -4,30 +4,34 @@
 import pandas as pd
 import dataAnalysis as da
 import helperFunctions as hf
-import numpy as np
 
 # Reading files
 trainDf = pd.read_csv('../Datasets/KaggleHousePrices/train.csv')
 testDf = pd.read_csv('../Datasets/KaggleHousePrices/test.csv')
-
+originalDf = testDf
 # EDA of the dataset
 da.EDA(trainDf, testDf)
+
 # Processing the variables that has NAs which means house does...
 # ...not have this feature or object Note: GarageYrBlt indicates...
 # ...the year when the garage is built but properties that does not...
 # ...have garage is empty and since year is categorical variable...
 # ...I filled the NAs with 'None'
 featuresNATrain = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'BsmtFinType2', 'BsmtExposure', 'BsmtFinType1', 'BsmtCond', 'BsmtQual', 'GarageYrBlt']
-hf.fillNANone(trainDf, featuresNATrain)
+trainDf = hf.fillNANone(trainDf, featuresNATrain)
 featuresNATest = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'BsmtFinType2', 'BsmtExposure', 'BsmtFinType1', 'BsmtCond', 'BsmtQual', 'GarageYrBlt']
-hf.fillNANone(testDf, featuresNATest)
+testDf = hf.fillNANone(testDf, featuresNATest)
+
 # Fill NAs with 'Other'
-hf.fillNAOther(trainDf, ['MasVnrType', 'MasVnrArea'])
-hf.fillNAOther(testDf, ['MasVnrType', 'MasVnrArea'])
+# TODO: build a model to predict MasVnrType and MasVnrArea
+trainDf = hf.fillNAOther(trainDf, ['MasVnrType'])
+testDf = hf.fillNAOther(testDf, ['MasVnrType'])
+trainDf = hf.fillNAZero(trainDf, ['MasVnrArea'])
+testDf = hf.fillNAZero(testDf, ['MasVnrArea'])
 
 # Fill NAs with most frequent value in these features
 # Note: These variables have missing values only in train set
-hf.fillWithMostFrequent(trainDf, ['Electrical'])
+trainDf = hf.fillWithMostFrequent(trainDf, ['Electrical'])
 
 # Note: These variables have missing values only in test set
 # Note: TotalBsmtSF is filled with 0 because, this house has NA for...
@@ -35,11 +39,11 @@ hf.fillWithMostFrequent(trainDf, ['Electrical'])
 # ...and houses with no basement has TotalBsmtSF of 0, thus this...
 # ...sample is also filled with 0
 # Note: Other variables in this list have also same issue
-hf.fillNAZero(testDf, ['TotalBsmtSF', 'GarageArea', 'GarageCars', 'BsmtUnfSF', 'BsmtFinSF2', 'BsmtFinSF1', 'BsmtHalfBath', 'BsmtFullBath'])
+testDf = hf.fillNAZero(testDf, ['TotalBsmtSF', 'GarageArea', 'GarageCars', 'BsmtUnfSF', 'BsmtFinSF2', 'BsmtFinSF1', 'BsmtHalfBath', 'BsmtFullBath'])
 
 # Fill NAs with most frequent value in these features
 # Note: These variables have missing values only in test set
-hf.fillWithMostFrequent(testDf, ['KitchenQual', 'SaleType', 'Exterior1st', 'Exterior2nd', 'Functional', 'Utilities', 'MSZoning'])
+testDf = hf.fillWithMostFrequent(testDf, ['KitchenQual', 'SaleType', 'Exterior1st', 'Exterior2nd', 'Functional', 'Utilities', 'MSZoning'])
 
 # Information about missing values
 da.infoMisVal(trainDf, trainDf.shape[0], "train")
@@ -51,17 +55,40 @@ testDf = testDf.drop(columns=['LotFrontage', 'Id'])
 # TODO: not handled: LotFrontage
 
 # Merge, encode, and split again
-# testDf['SalePrice'] = np.nan
 mergedDf = pd.concat([trainDf, testDf], ignore_index=True)
-mergedDf.to_csv("merged.csv", header=True, index=False)
-import time
-time.sleep(12121)
-mergedDf = hf.ordinalEncoding(mergedDf)
-
+mergedDf = mergedDf.astype(str)
+# These variables are ordinal encoded since they are non-numeric
+# Note: GarageYrBlt is numerical its NA values are replaced with...
+# ...'Other' and since it is a categorical values(years) it is also...
+# ...ordinal encoded
+varsToEncode = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+                'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
+                'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'ExterCond', 'MasVnrType', 'ExterQual',
+                'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
+                'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional',
+                'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive',
+                'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition', 'GarageYrBlt']
+mergedDf = hf.ordinalEncoding(mergedDf, varsToEncode)
 trainDf = mergedDf[0:trainDf.shape[0]]
 testDf = mergedDf[trainDf.shape[0]:mergedDf.shape[0]]
+# mergedDf.to_csv("mergedDf.csv", header=True, index=False)
 
+# Build a Gradient Boosting Model
 modelGB, predictionsGB = hf.gbModel(trainDf, testDf, 'SalePrice')
-print(predictionsGB)
-# print(hf.calcR2(actualDf['SalePrice'], predictionsGB))
-# Modelling TODO: uses lasso, ridge elasticnet, SVM, RF, GB
+# Create a submission file
+pd.DataFrame({'Id': originalDf['Id'], 'SalePrice': predictionsGB}).to_csv("submission.csv", header=True, index=False)
+
+print(f"R-squared of the model for in-sample data is {(100*modelGB.score(trainDf.drop('SalePrice', 1), trainDf['SalePrice'])).round(1)}%")
+# pd.DataFrame({'columns': trainDf.drop('SalePrice', 1).columns, 'importances': modelGB.feature_importances_}).to_csv("impotances.csv", header=True, index=False)
+
+# Build a Random Forest Model
+# modelRF, predictionsRF = hf.rfModel(trainDf, testDf, 'SalePrice')
+# # Create a submission file
+# pd.DataFrame({'Id': originalDf['Id'], 'SalePrice': predictionsRF}).to_csv("submission.csv", header=True, index=False)
+#
+# print(f"R-squared of the model for in-sample data is {(100*modelRF.score(trainDf.drop('SalePrice', 1), trainDf['SalePrice'])).round(1)}%")
+# # pd.DataFrame({'columns': trainDf.drop('SalePrice', 1).columns, 'importances': modelRF.feature_importances_}).to_csv("impotances.csv", header=True, index=False)
+
+# TODO: uses lasso, ridge elasticnet, SVM, RF, GB
+
+# TODO: hypertune
